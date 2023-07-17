9aff736f12e117d9c12d0799d18e10bee9045c0c
descartes640@gmail.com <descartes640@gmail.com>
2023-07-16 21:50:32 -0600

    problema de clasificacion

---
+++ Clasificacion.py
@@ -0,0 +1,7 @@
+from keras.models import Sequential 
+from keras.utils import np_utils
+from keras.layers import Dense 
+from keras.wrappers.scikit_learn import KerasClassifier 
+from sklearn.model_selection import cross_val_score,KFold
+from sklearn.preprocessing import LabelEncoder 
+import tensorflow
